cdff91ac69bad8325f8c3c3b188bf11fc019b796
Devesh Kantilal Gopal <deveshkantilalgopal@gmail.com>
2024-02-26 11:55:58 +0200

    Implementing Fibonacci series solution as a loop and a recursive function.

---
+++ FoundationTask2.php
@@ -0,0 +1,24 @@
+<?php
+//Recursive function
+    function Fibonacci($n)
+    {
+        //Base Case - includes termination condition
+        if ($n <= 1)
+        {
+            return $n;
+        }
+
+        //Recursive case
+        return Fibonacci($n - 1) + Fibonacci($n - 2); //Indices of the fibonacci sequence
+    }
+
+    $maxValue = 35;
+    $fibonacciSequence = []; //Storing the fibonacci sequence into an array
+
+    // For loop to generate the fibonacci sequence
+    for ($i = 0; Fibonacci($i) <= $maxValue; $i++)
+    {
+        $fibonacciSequence[] = Fibonacci($i); //adds a number to the fibonacci sequence array
+    }
+
+    echo "Fibonacci Sequence: " . implode(" ", $fibonacciSequence) . "\n";
