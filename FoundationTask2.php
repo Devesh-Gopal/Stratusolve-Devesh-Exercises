74feb6b4c5e8d0ad4b38f38a32eb84ad693bb6dc
Devesh Kantilal Gopal <deveshkantilalgopal@gmail.com>
2024-02-27 11:10:57 +0200

    While loop inserted

--- FoundationTask2.php
+++ FoundationTask2.php
@@ -2,11 +2,9 @@
 //program that generates and prints a fibonacci sequence up to a given maximum value.
 
 //Recursive function
-    function Fibonacci($n)
-    {
+    function Fibonacci($n) {
         //Base Case - includes termination condition
-        if ($n <= 1)
-        {
+        if ($n <= 1) {
             return $n;
         }
 
@@ -14,15 +12,14 @@
         return Fibonacci($n - 1) + Fibonacci($n - 2); //Indices of the fibonacci sequence
     }
 
-    $maxValue = 35;
+    $maxValue = 34;
     $fibonacciSequence = []; //Storing the fibonacci sequence into an array
 
-    // For loop to generate the fibonacci sequence
-    for ($i = 0; Fibonacci($i) <= $maxValue; $i++)
-    {
-        $fibonacciSequence[] = Fibonacci($i); //adds a number to the fibonacci sequence array
+    // While loop
+    $i = 0; // Sequence starts at 0.
+    while (Fibonacci($i) <= $maxValue) {
+        $fibonacciSequence[] = Fibonacci($i); // adds a number to the fibonacci sequence array
+        $i++;
     }
 
     echo "Fibonacci Sequence: " . implode(" ", $fibonacciSequence) . "\n";
-
-
